15d0a8ecdf6fd2d8db00a182c7c35027453f5386
Romulo Mesquita <romulomesquita.06@gmail.com>
2023-10-28 14:19:23 -0300

    termino de criação do service

--- lgpd-back/services/CourseService.js
+++ lgpd-back/services/CourseService.js
@@ -1,7 +1,32 @@
 import courseRepository from "../repositories/CourseRepository.js";
 
-const service = {
+const saveCourse = (courseModel) => {
+    return courseRepository.saveCourse(courseModel)
+}
 
+const getCourseById = (id) => {
+    return courseRepository.getCourseById(id)
+}
+
+const getAllCourses = () => {
+    return courseRepository.getAllCourses()
+}
+
+const deletCourseById = (id) => {
+    return courseRepository.deletCourseById(id)
+}
+
+const updateCourseById = (id, courseModel) => {
+    return courseRepository.updateCourseById(id, courseModel)
+}
+
+
+const service = {
+    saveCourse,
+    getCourseById,
+    getAllCourses,
+    deletCourseById,
+    updateCourseById
 }
 
 export default service;
--- lgpd-back/services/EvaluationService.js
+++ lgpd-back/services/EvaluationService.js
@@ -1,7 +1,31 @@
 import evaluationRepository from "../repositories/EvaluationRepository.js";
 
-const service = {
+const saveEvaluation = (evaluationModel) => {
+    return evaluationRepository.saveEvaluation(evaluationModel)
+}
 
+const getEvaluationById = (id) => {
+    return evaluationRepository.getEvaluationById(id)
+}
+
+const getAllEvaluations = () => {
+    return evaluationRepository.getAllEvaluations()
+}
+
+const deletEvaluationById = (id) => {
+    return evaluationRepository.deletEvaluationById(id)
+}
+
+const updateEvaluationById = (id, evaluationModel) => {
+    return evaluationRepository.updateEvaluationById(id, evaluationModel)
+}
+
+const service = {
+    saveEvaluation,
+    getEvaluationById,
+    getAllEvaluations,
+    deletEvaluationById,
+    updateEvaluationById
 }
 
 export default service;
